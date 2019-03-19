e20ff43d6122f7ae9ee7956dc9ff4d6425645a0d
Jeff Behnke <jeff@validwebs.com>
2019-03-19 16:06:33 -0500

    Cleanup

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *
+ * PHP version 5
+ *
+ * Created: 3/19/19, 4:05 PM
+ *
+ * LICENSE:
+ *
+ * @author         Jeff Behnke <jeff@indemna.com>
+ * @copyright  (c) 2019 Indemna.com
+ *
+ * mormvc
+ * index.php
+ */
+
+
+// End index.php
